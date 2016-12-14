1469c4946806b046bb0da5aa5f6a9b1d5c03fe39
Stone.lee <Stone.lee@LENOVO-201601>
2016-12-14 10:18:50 +0800

    加入專案檔案。

---
+++ MVC_TEST/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_TEST.Models;
+using MVC_TEST.Services;
+namespace MVC_TEST.Controllers
+{
+    public class MessageController : Controller
+    {
+        messageDBService data = new messageDBService();
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View(data.GetData());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string Article_title,string Content)
+        {
+            //塞入資料庫
+            data.DBCreate(Article_title, Content);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_TEST/Services/messageDBService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_TEST.Models;
+namespace MVC_TEST.Services
+{
+    public class messageDBService
+    {
+        public MVC_TEST.Models.messageEntities db = new Models.messageEntities();
+
+        public List<Article> GetData()
+        {
+            return (db.Article.ToList());
+        }
+        public void DBCreate(string Article_title,string Content)
+        {
+            Article NewData = new Article();
+            NewData.Title = Article_title;
+            NewData.Content = Content;
+            NewData.time = DateTime.Now;
+            db.Article.Add(NewData);
+            db.SaveChanges();
+        }
+    }
+}
